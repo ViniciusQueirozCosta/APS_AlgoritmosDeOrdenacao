245390906157361d757b3849a475192fb0b9a48a
Vinícius Queiroz Costa <viniciusqueirozcosta@outlook.com>
2018-11-08 14:15:28 -0200

    Desenvolvimento da Bateria de testes.
    A ação de bateria de testes faz, em sequência, 100 'runs' de cada um dos tipos de algoritmos de ordenação. O jTextBox é atualizado dinamicamente através da separação entre dois threads. É também informado uma média ao final das 100 execuções de cada algoritmo.

---
+++ Iris/src/iris/Iris.java
@@ -0,0 +1,9 @@
+package iris;
+
+public class Iris {
+    public static void main(String[] args) {
+        View.frmPrincipal frmP = new View.frmPrincipal(null, true);
+        frmP.setVisible(true);
+    }
+    
+}
